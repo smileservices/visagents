5117e442aa8a2e46ef5bd4ecea9ea5659185a79b
vladimir.gorea <vladimir.gorea@gmail.com>
2020-07-18 13:44:53 +0700

    frontend build for production

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     },
     entry: {
         // just create the react-bundle once
-        // 'static/js/react-bundle': ['./app/static/js/react.min.js', './app/static/js/react-dom.min.js'],
+        'static/js/react-bundle': ['./app/static/js/react.min.js', './app/static/js/react-dom.min.js'],
         // 'static/js/react-bundle': ['./app/static/js/react.development.js', './app/static/js/react-dom.development.js'],
         '/frontend/static/frontend/app': './app/frontend/src/homepage/App.js',
         '/dashboard/static/dashboard/dashboard': './app/dashboard/src/dashboard/App.js',
