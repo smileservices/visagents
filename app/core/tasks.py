548c1b9517fdfb55cdb684134c30feb3ba5ec047
vladimir.gorea <vladimir.gorea@gmail.com>
2020-07-25 17:18:29 +0700

    tasks.email - add exception handling

--- app/core/tasks.py
+++ app/core/tasks.py
@@ -11,17 +11,22 @@ def email_bg(subject: str, body: str, sender: str, destination: list, reply_to: 
     email = EmailMessage(
         subject, body, sender, destination, reply_to=reply_to, headers=headers
     )
-    response = email.send()
-    logger.info(
-        f'\n'
-        f'==================================================\n'
-        f'Sent mail to {destination} with subject {subject}:\n'
-        f'sender: {sender}, reply_to: {reply_to}, body:\n'
-        f'{body}\n'
-        f'==================================================\n'
-        f'response: {response}'
-        f'\n'
-    )
+    try:
+        logger.info(
+            f'\n'
+            f'\n'
+            f'Sending mail to {destination} with subject {subject}:\n'
+            f'sender: {sender}, reply_to: {reply_to}, body:\n'
+            f'{body}\n'
+            f'\n'
+            f'\n'
+        )
+        response = email.send()
+        logger.info(f'Got Response: {response}')
+        if response != 1:
+            raise Exception(f'Received response {response} from server')
+    except Exception as e:
+        logger.error(f'Ecountered error while trying to send email: \n{e}')
 
 
 @background
